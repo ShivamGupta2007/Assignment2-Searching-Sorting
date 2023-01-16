44e7aa8dd63f4170a9bb449a63fc20274d61409d
Shivam Gupta <100027945+ShivamGupta2007@users.noreply.github.com>
2023-01-16 16:13:09 +0530

    searching & sorting

---
+++ Search & Sorting/sortList.js
@@ -0,0 +1,7 @@
+const lst = ["Banana", "Orange", "Apple", "Mango"];
+document.write("<b>Given String: <br></b>");
+document.write(lst);
+//lst.sort();
+document.write("<br>");
+document.write("<b>Sort list String:<br></b>");
+document.write(lst.sort());
